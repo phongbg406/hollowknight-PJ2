a97128d44082b80a65c1d48bcf72464c12adbd2c
phongbg406 <phongbg406@gmail.com>
2023-08-15 14:07:32 +0700

    init project

---
+++ Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    private AudioSource audioPlayer;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayOneShot(AudioClip audioClip)
+    {
+        audioPlayer.PlayOneShot(audioClip);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SoundVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+}
